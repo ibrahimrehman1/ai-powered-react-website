de8006def853d94e2025c26abb3d274a7eac7e08
Ibrahim Rehman <ibrahimrehman0346@gmail.com>
2020-10-24 00:27:25 +0500

    Feedback Component Solved!

--- src/Components/feedbackComponent.js
+++ src/Components/feedbackComponent.js
@@ -13,19 +13,19 @@ export function Feedback(){
         <h1 style={{textAlign: "center"}}>Feedback Form</h1>
         <Form style={{marginTop: "50px"}} onSubmit={(event)=>{alert("Form has been Submitted!");event.preventDefault()}}>
             <Form.Group>
-                <Form.Label className="col-12 offset-md-3" style={{paddingLeft: "0px"}}>First Name</Form.Label>
+                <Form.Label className="col-12 offset-md-3 col-md-4" style={{paddingLeft: "0px"}}>First Name</Form.Label>
                 <Form.Control name="firstname" id="firstname" className="col-12 col-md-6 offset-md-3" type="text"/>
             </Form.Group>
             <Form.Group>
-                <Form.Label className="col-12 offset-md-3" style={{paddingLeft: "0px"}}>Last Name</Form.Label>
+                <Form.Label className="col-12 offset-md-3 col-md-4" style={{paddingLeft: "0px"}}>Last Name</Form.Label>
                 <Form.Control name="lastname" id="lastname" className="col-12 col-md-6 offset-md-3" type="text"/>
             </Form.Group>
             <Form.Group>
-                <Form.Label className="col-12 offset-md-3" style={{paddingLeft: "0px"}}>Email Address</Form.Label>
+                <Form.Label className="col-12 offset-md-3 col-md-4" style={{paddingLeft: "0px"}}>Email Address</Form.Label>
                 <Form.Control name="email" id="email" className="col-12 col-md-6 offset-md-3" type="email"/>
             </Form.Group>
             <Form.Group>
-                <Form.Label className="col-12 offset-md-3" style={{paddingLeft: "0px"}}>Feedback</Form.Label>
+                <Form.Label className="col-12 offset-md-3 col-md-4" style={{paddingLeft: "0px"}}>Feedback</Form.Label>
                 <Form.Control as="textarea" rows={8} className="col-12 col-md-6 offset-md-3"/>
             </Form.Group>
             <Form.Group className="text-center">
